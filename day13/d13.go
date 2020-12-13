94063ff5a4a7b3117575dcc80ed00fc13d1b1577
Vojtech Podhajsky <lameorc@gmail.com>
2020-12-13 22:38:32 +0100

    day 13 brute-force version, solved with wolfram...

--- day13/d13.go
+++ day13/d13.go
@@ -8,12 +8,13 @@ import (
 )
 
 type bus struct {
-	mask int
+	index int
+	mask  int
 }
 
 func newBuses(in string) ([]bus, error) {
 	b := make([]bus, 0)
-	for _, s := range strings.Split(in, ",") {
+	for i, s := range strings.Split(in, ",") {
 		if s == "x" {
 			continue
 		}
@@ -21,7 +22,7 @@ func newBuses(in string) ([]bus, error) {
 		if err != nil {
 			return nil, err
 		}
-		b = append(b, bus{val})
+		b = append(b, bus{index: i, mask: val})
 	}
 	return b, nil
 }
@@ -45,6 +46,30 @@ func (b *bus) untilNextFrom(t int) int {
 	return untilNext
 }
 
+func part2(buses []bus) int {
+
+	timestamp := 0
+	// The timestamp interval must be between first and last inclusive, otherwise the first
+	// leaves before the last one had a chance
+	for ; ; timestamp++ {
+		if timestamp%buses[0].mask != 0 {
+			continue
+		}
+		busesConverging := 0
+		for _, b := range buses {
+			if (timestamp+b.index)%b.mask == 0 {
+				busesConverging++
+			} else {
+				// no need now, we need all to match
+				break
+			}
+		}
+		if busesConverging == len(buses) {
+			return timestamp
+		}
+	}
+}
+
 func Solve(input []string) {
 	// first line is timestamp
 	earliest, err := strconv.Atoi(input[0])
@@ -59,4 +84,5 @@ func Solve(input []string) {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(earliest, buses))
+	fmt.Printf("Part 2: %d\n", part2(buses))
 }
